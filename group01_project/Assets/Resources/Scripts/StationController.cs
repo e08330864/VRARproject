d3fb7540efdf38dafafdf2de58b98a3d6358f25a
Horst <e08330864@student.tuwien.ac.at>
2019-11-28 15:51:21 +0100

    added stations to scene

---
+++ group01_project/Assets/Resources/Scripts/StationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationController : MonoBehaviour
+{
+    Transform station_head = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform child in transform)
+        {
+            if(child.tag == "StationHead")
+            {
+                station_head = child;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(station_head != null)
+        {
+            Vector3 scale = station_head.localScale;
+            float delta =  0.25f * Mathf.Sin(Time.time) + 2;
+            scale.x = delta;
+            scale.z = delta;
+            station_head.localScale = scale;
+        }
+    }
+}
