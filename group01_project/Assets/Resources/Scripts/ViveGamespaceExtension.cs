218d7696b0cb62bba70c347938c8355b3288aa83
Horst <e08330864@student.tuwien.ac.at>
2019-11-29 16:07:37 +0100

    Moved Vive Gamespace extension to own file

---
+++ group01_project/Assets/Resources/Scripts/ViveGamespaceExtension.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveGamespaceExtension : MonoBehaviour
+{
+    [HideInInspector]
+    public bool rightGrabPinch;
+    private bool lastRightGrabPinch;
+
+    Transform rightViveController = null;
+    Vector3? rightControllerPositionStart = null;
+    float gameSpaceExtensionSpeed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightViveController = GameObject.Find("Controller (right)").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastRightGrabPinch = rightGrabPinch;
+        rightGrabPinch = SteamVR_Actions._default.GrabPinch.GetState(SteamVR_Input_Sources.RightHand);
+
+        //game space extension
+        Vector3 shift = calculateGameSpaceShift();
+        transform.position += new Vector3(shift.x, 0.0f, shift.z);
+    }
+
+    Vector3 calculateGameSpaceShift()
+    {
+        Vector3 shift = new Vector3(0.0f, 0.0f, 0.0f);
+
+
+        //right Hand Controller started Grab Pinch
+        if (!lastRightGrabPinch && rightGrabPinch)
+        {
+            rightControllerPositionStart = rightViveController.position;
+        }
+
+        //right hands continus grab Pinch
+        if (lastRightGrabPinch && rightGrabPinch)
+        {
+            if (rightControllerPositionStart.HasValue)
+            {
+                shift = rightViveController.position - rightControllerPositionStart.Value;
+            }
+            else
+            {
+                Debug.Log("Trying to calculate pinch, but not pinch start operation yet detected");
+            }
+
+        }
+
+        //right hand stopped grab pinching
+        if (lastRightGrabPinch && !rightGrabPinch)
+        {
+            shift = new Vector3(0.0f, 0.0f, 0.0f);
+            rightControllerPositionStart = null;
+        }
+
+        return gameSpaceExtensionSpeed * shift;
+    }
+}
--- group01_project/Assets/Resources/Scripts/ViveGrab.cs
+++ group01_project/Assets/Resources/Scripts/ViveGrab.cs
@@ -16,33 +16,15 @@ public class ViveGrab : MonoBehaviour
     public Collider leftTouch, rightTouch;
     [HideInInspector]
     public bool leftGrabPinch, rightGrabPinch;
-    private bool lastLeftGrabPinch, lastRightGrabPinch;
 
     private Collider grabbedCollider;
-
-    Transform leftViveController = null;
-    Transform rightViveController = null;
-    Vector3? rightControllerPositionStart = null;
-    float gameSpaceExtensionSpeed = 0.0001f;
-
-    private void Start()
-    {
-        leftViveController = GameObject.Find("Controller (left)").transform;
-        rightViveController = GameObject.Find("Controller (right)").transform;
-    }
 
 
     // Update is called once per frame
     void Update()
     {
-        lastLeftGrabPinch = leftGrabPinch;
-        lastRightGrabPinch = rightGrabPinch;
         leftGrabPinch = SteamVR_Actions._default.GrabPinch.GetState(SteamVR_Input_Sources.LeftHand);
         rightGrabPinch = SteamVR_Actions._default.GrabPinch.GetState(SteamVR_Input_Sources.RightHand);
-
-        //game space extension
-        Vector3 shift = calculateGameSpaceShift();
-        transform.position += new Vector3(shift.x, 0.0f, shift.z);
 
         if (leftTouch != null && leftTouch == rightTouch && rightGrabPinch && leftGrabPinch)
         {
@@ -57,40 +39,5 @@ public class ViveGrab : MonoBehaviour
             grabbedCollider.gameObject.GetComponent<AuthorityManager>().grabbedByPlayer = false;
             grabbedCollider = null;
         }
-    }
-
-    Vector3 calculateGameSpaceShift()
-    {
-        Vector3 shift = new Vector3(0.0f, 0.0f, 0.0f);
-        
-
-        //right Hand Controller started Grab Pinch
-        if (!lastRightGrabPinch && rightGrabPinch)
-        {
-            rightControllerPositionStart = rightViveController.position;
-        }
-
-        //right hands continus grab Pinch
-        if (lastRightGrabPinch && rightGrabPinch)
-        {
-            if (rightControllerPositionStart.HasValue)
-            {
-                shift = rightViveController.position - rightControllerPositionStart.Value;
-            }
-            else
-            {
-                Debug.Log("Trying to calculate pinch, but not pinch start operation yet detected");
-            }
-            
-        }
-
-        //right hand stopped grab pinching
-        if(lastRightGrabPinch && !rightGrabPinch)
-        {
-            shift = new Vector3(0.0f, 0.0f, 0.0f);
-            rightControllerPositionStart = null;
-        }
-
-        return gameSpaceExtensionSpeed * shift;
     }
 }
