10cb209eae4b334c2654a2fe421b56c2de3beaa3
Horst <e08330864@student.tuwien.ac.at>
2019-12-06 21:27:50 +0100

    Leap movement 2

--- group01_project/Assets/Resources/Scripts/LeapGestures.cs
+++ group01_project/Assets/Resources/Scripts/LeapGestures.cs
@@ -11,10 +11,9 @@ public class LeapGestures : MonoBehaviour
     private Hand handLeft = null;
     private Hand handRight = null;
 
-    private int oldGestureStatus = 0;   // 0...at least one hand with no pistol
+    private int gestureStatus = 0;      // 0...at least one hand with no pistol
                                         // 1...both hands pistol untriggered
                                         // 2...both hands pistol triggered
-    private int newGestureStatus = 0;
     private bool doMove = false;
 
 
@@ -42,16 +41,55 @@ public class LeapGestures : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckMovement();
+        if (doMove)
+        {
+            DoMovement();
+        }
+    }
+
+    private void DoMovement()
+    {
+        Vector3 direction = UnityVectorExtension.ToVector3(handLeft.Fingers[1].Direction + handRight.Fingers[1].Direction).normalized;
     }
 
     private void CheckMovement()
     {
         int leftPG = CheckPistol(handLeft);
         int rightPG = CheckPistol(handRight);
-        switch (oldGestureStatus)
+        switch (gestureStatus)
         {
             case 0: // at least one hand with no pistol
+                if (leftPG == 1 && rightPG == 1)
+                {
+                    gestureStatus = 1;
+                }
+                if (leftPG == 2 && rightPG == 2)
+                {
+                    gestureStatus = 2;
+                }
+                break;
+            case 1: // both hands pistol untriggered
+                if (leftPG == 0 || rightPG == 0)
+                {
+                    gestureStatus = 0;
+                }
+                if (leftPG == 2 && rightPG == 2)
+                {
+                    gestureStatus = 2;
+                    doMove = true;
+                }
+                break;
+            case 2: // 2...both hands pistol triggered
+                if (leftPG == 0 || rightPG == 0)
+                {
+                    gestureStatus = 0;
+                }
+                if (leftPG == 1 && rightPG == 1)
+                {
+                    gestureStatus = 1;
+                    doMove = true;
+                }
                 break;
         }
     }
