30315d83c026b7637aeec87505f0ad35a97dd8a8
Horst <e08330864@student.tuwien.ac.at>
2019-12-06 10:31:00 +0100

    OnGrabbedBehavior GetHands

--- group01_project/Assets/Resources/Scripts/OnGrabbedBehaviour.cs
+++ group01_project/Assets/Resources/Scripts/OnGrabbedBehaviour.cs
@@ -13,29 +13,21 @@ public class OnGrabbedBehaviour : MonoBehaviour
     AuthorityManager authorityManager = null;
     GameObject handLeft = null;
     GameObject handRight = null;
+    Actor actor = null;
 
     // Use this for initialization
     void Start ()
     {
         if ((authorityManager = GetComponent<AuthorityManager>()) == null) { Debug.LogError("authorityManager is NULL in OnGrabbedBehaviour"); }
         if ((rigidbody = GetComponent<Rigidbody>()) == null) { Debug.LogError("rigidbody is NULL in OnGrabbedBehaviour"); }
-        if ((netId = GetComponent<NetworkIdentity>()) == null) { Debug.LogError("netId is NULL in OnGrabbedBehaviour"); }
-        if (authorityManager.GetActor().gameObject.transform.parent.tag == "vive")
-        {
-            handLeft = authorityManager.GetActor().transform.Find("ViveHands/Left").gameObject;
-            handRight = authorityManager.GetActor().transform.Find("ViveHands/Right").gameObject;
-        }
-        else if (authorityManager.GetActor().gameObject.transform.parent.tag == "leap")
-        {
-            handLeft = GameObject.FindGameObjectWithTag("LeftHandInteraction");
-            handRight = GameObject.FindGameObjectWithTag("RightHandInteraction");
-        }
+        if ((netId = GetComponent<NetworkIdentity>()) == null) { Debug.LogError("netId is NULL in OnGrabbedBehaviour"); } 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (held)
+        GetHands();
+        if (handLeft != null && handRight != null && held)
         {
             // Debug.Log("OnGrabbedBehaviour: held = true");
             if (authorityManager.GetActor() == null || !netId.hasAuthority) return;
@@ -48,47 +40,6 @@ public class OnGrabbedBehaviour : MonoBehaviour
             {
                 this.transform.position = handRight.transform.position;
             }
-            //if (authorityManager.GetActor().gameObject.transform.parent.tag == "vive")   // VIVE grabbed
-            //{
-            //    if (authorityManager.GetLeftGrabbed())  // Left hand
-            //    {
-            //        GameObject hand = authorityManager.GetActor().transform.Find("ViveHands/Left").gameObject;
-            //        if (hand)
-            //        {
-            //            this.transform.position = hand.transform.position;
-            //        }
-            //    }
-            //    else    //Right hand
-            //    {
-            //        GameObject hand = authorityManager.GetActor().transform.Find("ViveHands/Right").gameObject;
-            //        if (hand)
-            //        {
-            //            this.transform.position = hand.transform.position;
-            //        }
-            //    }
-            //}
-            //else if (authorityManager.GetActor().gameObject.transform.parent.tag == "leap")  // Leap grabbed
-            //{
-            //    // Debug.Log("OnGrabbedBehaviour: grabbed = true");
-            //    if (authorityManager.GetLeftGrabbed())  // Left hand
-            //    {
-            //        GameObject hand = GameObject.FindGameObjectWithTag("LeftHandInteraction");
-            //        if (hand)
-            //        {
-            //            // Debug.Log("OnGrabbedBehaviour: Left-Hand grabbed move object to position=" + hand.transform.position);
-            //            this.transform.position = hand.transform.position;
-            //        }
-            //    }
-            //    else    //Right hand
-            //    {
-            //        GameObject hand = GameObject.FindGameObjectWithTag("RightHandInteraction");
-            //        if (hand)
-            //        {
-            //            // Debug.Log("OnGrabbedBehaviour: Right-Hand grabbed move object to position=" + hand.transform.position);
-            //            this.transform.position = hand.transform.position;
-            //        }
-            //    }
-            //}
         }
     }
 
@@ -109,5 +60,22 @@ public class OnGrabbedBehaviour : MonoBehaviour
         held = false;
         rigidbody.isKinematic = false;
         rigidbody.useGravity = true;
+    }
+
+    private void GetHands()
+    {
+        if (actor == null && (actor = authorityManager.GetActor()) != null)
+        {
+            if (actor.gameObject.transform.parent.tag == "vive")
+            {
+                handLeft = authorityManager.GetActor().transform.Find("ViveHands/Left").gameObject;
+                handRight = authorityManager.GetActor().transform.Find("ViveHands/Right").gameObject;
+            }
+            else if (actor.gameObject.transform.parent.tag == "leap")
+            {
+                handLeft = GameObject.FindGameObjectWithTag("LeftHandInteraction");
+                handRight = GameObject.FindGameObjectWithTag("RightHandInteraction");
+            }
+        }
     }
 }
